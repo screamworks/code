e0d1673022bd6188a350cc15274a4424da79714a
shiva patel <shivapatel15@yahoo.com>
2014-02-05 01:13:56 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PeopleCode::Application.config.secret_key_base = '952da8dbdc8d1b82c2406e15ce6e6d258c7d7080564e5ea772b0f73b9773596f565c95f3c6ea8e09a18436082b24183c3cf038b849041a9773fa871e6b8dd1f2'
