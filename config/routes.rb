5f38e1f90498b8aae9b3ef98659dc086dffb0585
shiva patel <shivapatel15@yahoo.com>
2014-02-05 04:35:48 -0500

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 PeopleCode::Application.routes.draw do
+  devise_for :users
+  get "static_pages/contact"
+
+  root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
